881d49cf2668f1263a8e72b440d881621cdd37b8
Sergei Zabolotskikh <serggzz@gmail.com>
2015-10-06 13:44:03 +0200

    add libadblockplus apple version

---
+++ apple/libadblockplus/libadblockplus-osx/libadblockplus_osx.m
@@ -0,0 +1,13 @@
+//
+//  libadblockplus_osx.m
+//  libadblockplus-osx
+//
+//  Created by Sergei Zabolotskikh on 10/09/15.
+//  Copyright © 2015 Eyeo GmbH. All rights reserved.
+//
+
+#import "libadblockplus_osx.h"
+
+@implementation libadblockplus_osx
+
+@end
---
+++ apple/libadblockplus/libadblockplus/libadblockplus.h
@@ -0,0 +1,13 @@
+//
+//  libadblockplus.h
+//  libadblockplus
+//
+//  Created by Sergei Zabolotskikh on 10/09/15.
+//  Copyright © 2015 Eyeo GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libadblockplus : NSObject
+
+@end
